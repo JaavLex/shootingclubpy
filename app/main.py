0c353c21abc6bd66588e27544549ccb53286eeb2
TacticsCH <alexandre.javet@hotmail.com>
2020-03-09 08:56:11 +0100

    Attempting to show HTML

--- app/main.py
+++ app/main.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Flask in a uWSGI Nginx Docker container with \
-     Python 3.7 (from the example template)"
+    return render_template("./templates/home.html")
 
 
 if __name__ == "__main__":
