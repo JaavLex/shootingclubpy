714b04bb0d04eca0d10fd98e924e116f9ab9fe7f
Nicolas Borboën <ponsfrilus@gmail.com>
2020-03-09 15:44:11 +0100

    [FT.] Default home with template base and home

--- web/app/views.py
+++ web/app/views.py
@@ -1,14 +1,14 @@
 from flask import render_template
 from app import app
 
-@app.route('/')
-def home():
+@app.route('/default')
+def default():
     return "Hello world!"
 
 @app.route('/api')
 def api():
     return {'hello': 'world'}
 
-@app.route('/html')
-def template():
-    return render_template('home.html')
+@app.route('/')
+def index():
+    return render_template('home.html', title='Hello World')
